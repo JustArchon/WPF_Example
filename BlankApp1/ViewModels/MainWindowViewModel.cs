428d966fea772cd444f86922ff749eced8ad753f
강규진 <puppy1900@naver.com>
2024-04-18 15:27:02 +0900

    first commit

---
+++ BlankApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+
+namespace BlankApp1.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        //private string _title = "Prism Application";
+        //public string Title
+        //{
+        //    get { return _title; }
+        //    set { SetProperty(ref _title, value); }
+        //}
+
+        public string Title { get; set; } = "Prism Application";
+        public DateTime FromDate { get; set; } = DateTime.Now.AddDays(-10);
+        public DateTime ToDate { get; set; } = DateTime.Now;
+
+
+        private DelegateCommand _cmdView;
+        public DelegateCommand CmdView =>
+            _cmdView ?? (_cmdView = new DelegateCommand(ExecuteCmdView));
+
+        void ExecuteCmdView()
+        {
+            Title = DateTime.Now.ToString();
+            RaisePropertyChanged(nameof(Title));
+        }
+        
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
